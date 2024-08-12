d072333f10204cc756b64a8504acd3138546ad59
tahnil <genosse.stanil@me.com>
2024-08-12 18:20:56 +0200

    removed some logging

--- app/src/utils/generateUniqueId.ts
+++ app/src/utils/generateUniqueId.ts
@@ -3,6 +3,6 @@
 export const generateUniqueId = () => {
     const timestamp = Date.now();
     const randomString = Math.random().toString(36).substring(2, 15);
-    console.log('[generateUniqueId] generated new unique id: ',timestamp,'-',randomString);
+    // console.log('[generateUniqueId] generated new unique id: ',timestamp,'-',randomString);
     return `${timestamp}-${randomString}`;
 };
