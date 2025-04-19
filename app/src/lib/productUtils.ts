9fd9e0c80eb910d0e247242c5bf30c1d48f302e5
Tahnil <genosse.stanil@me.com>
2025-04-20 00:12:30 +0200

    refactor(productUtils): enhance error handling and type definitions for clarity

--- app/src/lib/productUtils.ts
+++ app/src/lib/productUtils.ts
@@ -1,35 +1,48 @@
 // lib/productUtils.ts
-
-import { Product, Process, SpectralType, ProductWithSpectralTypes } from '../types/types';
 import { loadProductionChains } from './dataLoader';
-import { InfluenceProduct } from '@/types/influenceTypes';
+import { InfluenceProduct, InfluenceProcess, SpectralType, ProductWithSpectralTypes } from '@/types/influenceTypes';
 
 // Load the production chains data
 const productionChains = loadProductionChains();
 
 // Helper functions
-const getProductById = (id: string): Product | undefined => {
-  return productionChains.products.find(product => product.id === id);
+const getProductById = (id: string): InfluenceProduct => {
+  const product = productionChains.products.find(product => product.id === id);
+  if (!product) {
+    throw new Error(`[productUtils.ts] Product with ID "${id}" not found.`);
+  }
+  return product;
 };
 
-const getProcessesByProductId = (productId: string): Process[] => {
-  return productionChains.processes.filter(process =>
-    process.outputs.some(output => output.productId === productId)
-  );
+const getProcessesByProductId = (productId: string): InfluenceProcess[] => {
+  const process = productionChains.processes
+    .filter(process =>
+      process.outputs.some(output => output.productId === productId));
+  if (!process) {
+    throw new Error(`[productUtils.ts] Process with product ID "${productId}" not found.`);
+  }
+  return process;
 };
 
 const getSpectralTypesByProcessIds = (processIds: string[]): SpectralType[] => {
-  const spectralTypes = productionChains.spectralTypes ?? [];
-  return spectralTypes.filter(spectralType =>
-    spectralType.processes.some(processId => processIds.includes(processId))
-  );
+  if (!productionChains.spectralTypes) {
+    throw new Error('[productUtils.ts] No spectral types found.');
+  }
+  const spectralTypes = productionChains.spectralTypes
+    .filter(spectralType =>
+      spectralType.processes.some(processId => processIds.includes(processId))
+    )
+  if (!spectralTypes) {
+    throw new Error('[productUtils.ts] No spectral types found.');
+  }
+  return spectralTypes;
 };
 
 // Public API functions
-export const fetchProductById = async (id: string): Promise<ProductWithSpectralTypes | undefined> => {
+export const fetchProductById = async (id: string): Promise<ProductWithSpectralTypes> => {
   const product = getProductById(id);
   if (!product) {
-    return undefined;
+    throw new Error(`[productUtils.ts > fetchProductById] Product with ID "${id}" not found.`);
   }
 
   if (product.type === 'Raw Material') {
